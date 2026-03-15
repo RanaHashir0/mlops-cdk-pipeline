4da56879bac0308bf1fe6aec38c010071c02bd23
Rana Hashir <ranahashir4601@gmail.com.com>
2026-03-15 15:45:03 +0500

    Remove all files

--- app.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-from aws_cdk import App
-from stacks.mlops_stack import MLOpsStack
-
-app = App()
-MLOpsStack(app, 'MLOpsStack')
-app.synth()
--- lambda/delete.py
+++
@@ -1,9 +0,0 @@
-import boto3
-
-sagemaker = boto3.client('sagemaker')
-
-def handler(event, context):
-    sagemaker.delete_endpoint(EndpointName=event['Endpoint']['EndpointName'])
-    sagemaker.delete_endpoint_config(EndpointConfigName=event['EndpointConfig']['EndpointConfigName'])
-    sagemaker.delete_model(ModelName=event['Model']['ModelName'])
-    return {'status': 'cleanup initiated'}
